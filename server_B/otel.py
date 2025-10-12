034597c7c0d54685150da28cc4d3f9e5321c18cd
italo998carvalho <italo.998.carvalho@gmail.com>
2025-10-12 19:36:03 -0300

    Set default otel config for server b

---
+++ server_B/otel.py
@@ -0,0 +1,34 @@
+from functools import wraps
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace.export import (
+    BatchSpanProcessor,
+    ConsoleSpanExporter,
+)
+from opentelemetry.trace import Status, StatusCode
+
+resource = Resource.create({
+    "service.name": "server-B"
+})
+
+provider = TracerProvider(resource=resource)
+processor = BatchSpanProcessor(ConsoleSpanExporter())
+provider.add_span_processor(processor)
+
+trace.set_tracer_provider(provider)
+
+tracer = trace.get_tracer("server.b")
+
+def start_span(name):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            tracer = trace.get_tracer(__name__)
+            with tracer.start_as_current_span(name) as span:
+                result = func(*args, **kwargs)
+                span.set_status(Status(StatusCode.OK))
+                return result
+        return wrapper
+    return decorator
+
--- server_B/run.py
+++ server_B/run.py
@@ -2,6 +2,9 @@ import uvicorn
 from typing import Union
 from fastapi import FastAPI, Response, status
 from pydantic import BaseModel
+from otel import start_span
+from opentelemetry.trace import Status, StatusCode
+from opentelemetry import trace
 
 app = FastAPI(port=8001)
 
@@ -13,45 +16,52 @@ class Item(BaseModel):
 
 item_list = {}
 
-@app.get("/")
+@app.get('/')
 def read_root():
-    return {"Server": "B"}
+    return {'Server': 'B'}
 
-@app.get("/items")
+@app.get('/items')
+@start_span('list-items')
 def list_items():
+    current_span = trace.get_current_span()
+    current_span.set_status(Status(StatusCode.OK))
     return [x.model_dump() for x in item_list.values()]
 
-@app.get("/items/{item_id}")
+@app.get('/items/{item_id}')
+@start_span('read-item')
 def read_item(item_id: int, response: Response):
     if item_id in item_list:
         return item_list.get(item_id)
     else:
         response.status_code = status.HTTP_404_NOT_FOUND
-        return {"status": "not found"}
+        return {'status': 'not found'}
 
-@app.put("/items/{item_id}")
+@app.put('/items/{item_id}')
+@start_span('update-item')
 def update_item(item_id: int, item: Item, response: Response):
     if item_id in item_list:
         item.item_id = item_id
         item_list[item_id] = item
-        return {"status": "updated"}
+        return {'status': 'updated'}
     else:
         response.status_code = status.HTTP_404_NOT_FOUND
-        return {"status": "not found"}
+        return {'status': 'not found'}
 
-@app.post("/items", status_code=201)
-def update_item(item: Item):
+@app.post('/items', status_code=201)
+@start_span('create-item')
+def create_item(item: Item):
     item_list[item.item_id] = item
-    return {"status": "created"}
+    return {'status': 'created'}
 
-@app.delete("/items/{item_id}")
+@app.delete('/items/{item_id}')
+@start_span('remove-item')
 def remove_item(item_id: int, response: Response):
     if item_id in item_list:
         item_list.pop(item_id)
-        return {"status": "deleted"}
+        return {'status': 'deleted'}
     else:
         response.status_code = status.HTTP_404_NOT_FOUND
-        return {"status": "not found"}
+        return {'status': 'not found'}
     
-if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8001)
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8001)
