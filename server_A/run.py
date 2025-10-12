d9ddf949f5de64c771bb6a146d5159c2e1ac6b4d
italo998carvalho <italo.998.carvalho@gmail.com>
2025-10-12 19:42:54 -0300

    Set default otel config for server a

--- server_A/run.py
+++ server_A/run.py
@@ -3,6 +3,7 @@ from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
 import requests
+from otel import start_span
 
 app = FastAPI()
 base_url = 'http://127.0.0.1:8001'
@@ -13,47 +14,52 @@ class Item(BaseModel):
     price: float
     is_offer: Union[bool, None] = None
 
-@app.get("/")
-async def read_root():
-    return {"Server": "A"}
+@app.get('/')
+def read_root():
+    return {'Server': 'A'}
 
-@app.get("/items")
-async def list_items():
+@app.get('/items')
+@start_span('list-items')
+def list_items():
     endpoint = base_url + '/items'
     r = requests.get(endpoint)
     return r.json()
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
+@app.get('/items/{item_id}')
+@start_span('read-item')
+def read_item(item_id: int, q: Union[str, None] = None):
     endpoint = base_url + f'/items/{item_id}'
     r = requests.get(endpoint)
     return r.json()
 
-@app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item):
+@app.put('/items/{item_id}')
+@start_span('update-item')
+def update_item(item_id: int, item: Item):
     endpoint = base_url + f'/items/{item_id}'
     r = requests.put(endpoint, json=_build_payload(item))
     return r.json()
 
-@app.post("/items")
-async def update_item(item: Item):
+@app.post('/items')
+@start_span('create-item')
+def create_item(item: Item):
     endpoint = base_url + f'/items'
     r = requests.post(endpoint, json=_build_payload(item))
     return r.json()
 
-@app.delete("/items/{item_id}")
-async def remove_item(item_id: int):
+@app.delete('/items/{item_id}')
+@start_span('remove-item')
+def remove_item(item_id: int):
     endpoint = base_url + f'/items/{item_id}'
     r = requests.delete(endpoint)
     return r.json()
 
 def _build_payload(item: Item) -> dict[str, any]:
     return {
-        "item_id": item.item_id,
-        "name": item.name,
-        "price": item.price,
-        "is_offer": item.is_offer
+        'item_id': item.item_id,
+        'name': item.name,
+        'price': item.price,
+        'is_offer': item.is_offer
     }
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
