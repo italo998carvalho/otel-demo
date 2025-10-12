a94053910eb2b49009a90cb6841353b3473e68a8
Italo Carvalho <italo.998.carvalho@gmail.com>
2025-10-12 18:19:56 -0300

    Format response payloads

--- server_B/run.py
+++ server_B/run.py
@@ -1,6 +1,6 @@
 import uvicorn
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status
 from pydantic import BaseModel
 
 app = FastAPI(port=8001)
@@ -22,27 +22,36 @@ def list_items():
     return [x.model_dump() for x in item_list.values()]
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int):
-    return item_list.get(item_id, {})
+def read_item(item_id: int, response: Response):
+    if item_id in item_list:
+        return item_list.get(item_id)
+    else:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"status": "not found"}
 
 @app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    item.item_id = item_id
-    item_list[item_id] = item
-    return "updated"
+def update_item(item_id: int, item: Item, response: Response):
+    if item_id in item_list:
+        item.item_id = item_id
+        item_list[item_id] = item
+        return {"status": "updated"}
+    else:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"status": "not found"}
 
-@app.post("/items")
+@app.post("/items", status_code=201)
 def update_item(item: Item):
     item_list[item.item_id] = item
-    return "created"
+    return {"status": "created"}
 
 @app.delete("/items/{item_id}")
-def remove_item(item_id: int):
+def remove_item(item_id: int, response: Response):
     if item_id in item_list:
         item_list.pop(item_id)
-        return "deleted"
+        return {"status": "deleted"}
     else:
-        return "not found"
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"status": "not found"}
     
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8001)
