2f102ef153c53c88447bf39f7c9ed7f46c07ffc6
italo998carvalho <italo.998.carvalho@gmail.com>
2025-10-12 20:05:28 -0300

    Pass server b status code to server a responses

--- server_A/run.py
+++ server_A/run.py
@@ -1,6 +1,6 @@
 import uvicorn
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from pydantic import BaseModel
 import requests
 from otel import start_span
@@ -20,37 +20,47 @@ def read_root():
 
 @app.get('/items')
 @start_span('list-items')
-def list_items():
+def list_items(response: Response):
     endpoint = base_url + '/items'
     r = requests.get(endpoint)
+    
+    response.status_code = r.status_code
     return r.json()
 
 @app.get('/items/{item_id}')
 @start_span('read-item')
-def read_item(item_id: int, q: Union[str, None] = None):
+def read_item(item_id: int, response: Response):
     endpoint = base_url + f'/items/{item_id}'
     r = requests.get(endpoint)
+
+    response.status_code = r.status_code
     return r.json()
 
 @app.put('/items/{item_id}')
 @start_span('update-item')
-def update_item(item_id: int, item: Item):
+def update_item(item_id: int, item: Item, response: Response):
     endpoint = base_url + f'/items/{item_id}'
     r = requests.put(endpoint, json=_build_payload(item))
+
+    response.status_code = r.status_code
     return r.json()
 
 @app.post('/items')
 @start_span('create-item')
-def create_item(item: Item):
+def create_item(item: Item, response: Response):
     endpoint = base_url + f'/items'
     r = requests.post(endpoint, json=_build_payload(item))
+
+    response.status_code = r.status_code
     return r.json()
 
 @app.delete('/items/{item_id}')
 @start_span('remove-item')
-def remove_item(item_id: int):
+def remove_item(item_id: int, response: Response):
     endpoint = base_url + f'/items/{item_id}'
     r = requests.delete(endpoint)
+
+    response.status_code = r.status_code
     return r.json()
 
 def _build_payload(item: Item) -> dict[str, any]:
