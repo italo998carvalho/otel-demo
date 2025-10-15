39ad0c92db7bd3e91a4f323252ddc3c96046dff4
Italo Carvalho <italo.998.carvalho@gmail.com>
2025-10-15 17:52:00 -0300

    Config otel to send metrics to prometheus

--- server/otel.py
+++ server/otel.py
@@ -1,5 +1,6 @@
 from functools import wraps
 from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace.export import (
@@ -13,7 +14,7 @@ resource = Resource.create({
 })
 
 provider = TracerProvider(resource=resource)
-processor = BatchSpanProcessor(ConsoleSpanExporter())
+processor = BatchSpanProcessor(OTLPSpanExporter(endpoint="http://localhost:4318/v1/traces"))
 provider.add_span_processor(processor)
 
 trace.set_tracer_provider(provider)
