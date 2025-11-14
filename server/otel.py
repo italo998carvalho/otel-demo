b2c8fe76bd735f37056bdd23303507a4aa8778eb
Italo Carvalho <italo.998.carvalho@gmail.com>
2025-11-14 15:13:18 -0300

    Simplify trace id forward

--- server/otel.py
+++ server/otel.py
@@ -5,7 +5,6 @@ from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.trace import Status, StatusCode
-from opentelemetry.propagate import extract
 
 resource = Resource.create({
     'service.name': 'application-server'
@@ -24,18 +23,10 @@ def start_span(name):
         @wraps(func)
         def wrapper(*args, **kwargs):
             tracer = trace.get_tracer(__name__)
-            ctx = _extract_context(kwargs)
-            with tracer.start_as_current_span(name, context=ctx) as span:
+            with tracer.start_as_current_span(name) as span:
                 result = func(*args, **kwargs)
                 if span.status.status_code == StatusCode.UNSET:
                     span.set_status(Status(StatusCode.OK))
                 return result
         return wrapper
     return decorator
-
-def _extract_context(wrapper_args):
-    request = wrapper_args.get('request', None)
-    if request is not None:
-        return extract(request.headers)
-    else:
-        return None
